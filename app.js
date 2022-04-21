89d17f048494e81b7d746697170d8be5dac8d0f6
Joe Naples <jnaples90@gmail.com>
2022-04-21 18:13:21 -0400

    Adding logic to form

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('hello');
+// Selectors
+let userWeight = document.querySelector("#user-weight");
+const submitButton = document.querySelector(".form-button");
+
+// Event Listeners
+submitButton.addEventListener("click", averageWeight);
+
+// Functions
+function averageWeight(event) {
+   event.preventDefault();
+   console.log(userWeight.value);
+   let userWeight = [];
+   // get user input from table cell
+   // push into array and total up the sum
+   // cl the sum
+   // display sum in average weight box
+}
