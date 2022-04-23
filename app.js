bb23a38d4a9716f9ba43580ebbd696935d35ee6f
Joe Naples <jnaples90@gmail.com>
2022-04-23 10:53:04 -0400

    Added logic to sum up average of weight

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
 // Selectors
-let userWeight = document.querySelector("#user-weight");
-const submitButton = document.querySelector(".form-button");
+let userWeight = document.querySelector('#user-weight');
+let userWeight2 = document.querySelector('#user-weight2');
+let userWeight3 = document.querySelector('#user-weight3');
+let userWeight4 = document.querySelector('#user-weight4');
+let userWeight5 = document.querySelector('#user-weight5');
+let userWeight6 = document.querySelector('#user-weight6');
+let userWeight7 = document.querySelector('#user-weight7');
+const submitButton = document.querySelector('.form-button');
+const userWeightInput = document.querySelector('.userWeightInput');
 
 // Event Listeners
-submitButton.addEventListener("click", averageWeight);
+// submitButton.addEventListener('click', averageWeight);
+userWeight.addEventListener('keyup', averageWeight);
 
 // Functions
 function averageWeight(event) {
-   event.preventDefault();
-   console.log(userWeight.value);
-   let userWeight = [];
-   // get user input from table cell
-   // push into array and total up the sum
-   // cl the sum
-   // display sum in average weight box
+    let userWeightArr = [];
+    // push user values into array
+
+    userWeightArr.push(parseInt(userWeight.value));
+    userWeightArr.push(parseInt(userWeight2.value));
+    userWeightArr.push(parseInt(userWeight3.value));
+    userWeightArr.push(parseInt(userWeight4.value));
+    userWeightArr.push(parseInt(userWeight5.value));
+    userWeightArr.push(parseInt(userWeight6.value));
+    userWeightArr.push(parseInt(userWeight7.value));
+
+    console.log(userWeightArr);
+    // Sum all numbers in an array
+    const sum = userWeightArr.reduce((a, b) => a + b, 0);
+    console.log(sum);
+    // Average all numbers by array.length
+    const average = sum / userWeightArr.length;
+    console.log(average);
+    // Display average on screen to user
+    userWeightInput.innerText = average;
 }
